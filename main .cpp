f1d90e5654a6af8148c4efe5422e9d911e50c15b
jk-aero <92267875+jk-aero@users.noreply.github.com>
2024-07-27 13:18:50 +0530

    Update main .cpp

--- main .cpp
+++ main .cpp
@@ -44,3 +44,117 @@ Vector2 normalise(float x, float y) {
     return Vector2{ x / length, y / length };
 }
 
+
+int main() {
+
+    // Initialization
+    //--------------------------------------------------------------------------------------
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    for (int i = 0; i < numOfCellX; i++) {
+        for (int j = 0; j < numOfCellY; j++)
+        {
+            grids[j * numOfCellX + i] = Grid{ i * cellSize,j * cellSize };
+         
+
+        }
+    };
+
+    bool setStart = 1;
+    bool setEnd = 1;
+    float startX = 0; float startY = 0;
+
+    float endX = 380; float endY = 250;
+ 
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+         int mouse_x = GetMousePosition().x;
+         int mouse_y = GetMousePosition().y;
+
+ 
+        
+         Raycast(mouse_x, mouse_y, endX, endY);
+
+        
+     
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+        ClearBackground(BLACK);
+
+        
+
+        for (int x = 0; x < screenWidth; x += cellSize) {
+            for (int y = 0; y < screenHeight; y += cellSize) {
+                
+                DrawCircle(mouse_x, mouse_y, 10, GREEN);//player
+                DrawCircle(endX, endY, 10, ORANGE);// ending postion
+
+                if(IsKeyDown(KEY_W)) {endY -= 0.05;}
+                if (IsKeyDown(KEY_S)){endY += 0.05;}
+                if (IsKeyDown(KEY_A)){endX -= 0.05;}
+                if (IsKeyDown(KEY_D)){endX += 0.05;}
+                if (endX < 0) { endX = 0; };
+                if (endY < 0) { endY = 0; }
+                if (endX >= 600) { endX = 600; }
+                if (endY >= 600) { endY = 600; }
+
+                if ((mouse_x >= x && mouse_x <= x + cellSize) && (mouse_y >= y && mouse_y <= y + cellSize))
+                {
+                   
+                    if (IsMouseButtonReleased(1)) {
+                        grids[(x / cellSize) + (y/ cellSize) * numOfCellY].type = !grids[(x/ cellSize) + (y/ cellSize) * numOfCellY].type;
+                        if (grids[(x/ cellSize) + (y/ cellSize) * numOfCellY].type == 0) { grids[(x / cellSize) + (y / cellSize) * numOfCellY].Color = GRAY; }
+                        else if (grids[(x / cellSize) + (y / cellSize) * numOfCellY].type == 1) { grids[(x / cellSize) + (y/ cellSize) * numOfCellY].Color = DARKPURPLE; }
+                    }
+                }
+
+
+
+
+
+
+
+                DrawRectangle(x, y, cellSize - 1, cellSize - 1, grids[(x / cellSize) + (y / cellSize) * numOfCellY].Color);
+                if (setStart && setEnd)
+                {
+               
+                    if (IsMouseButtonDown(0)) {
+                        DrawLine(mouse_x, mouse_y, endX, endY, BLUE);
+                        //std::cout << "mouse Postion x:" << mouse_x << "  Y:" << mouse_y << "  endx:" << endX << "   endY:" << endY << '\n';
+                        /*if (Tilefound)
+                        {
+                            DrawCircleV(intersection, 5, ORANGE);
+                            std::cout << intersection.x << " " << intersection.y << '\n';
+
+
+                        }*/
+                    }
+                }
+            }
+        }
+        
+
+
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+
+
+
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+}
